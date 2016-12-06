d156b184f92039ad8d910e2202e191e2722c6d1e
Lisandro Lorea <lisandrolorea@gmail.com>
2016-12-06 05:32:20 -0300

    cohete changes pitch based on thrust slider value

--- scripts/cohete.gd
+++ scripts/cohete.gd
@@ -3,6 +3,10 @@ extends SamplePlayer2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+onready var thrust = get_parent().get_node("Camera2D/Thrust")
+export var base_pitch_scale = 0.5
+export var pitch_scale_multiplier = 2.0
+
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -10,6 +14,7 @@ func _ready():
 	set_process(true)
 	
 func _process(delta):
+	voice_set_pitch_scale(0, base_pitch_scale + pitch_scale_multiplier * thrust.get_value())
 	if (!is_voice_active(0)):
 		play('thrust')
 
